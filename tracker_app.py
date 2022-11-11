d4d3f0c191db2d15165648e3d6791a1dca6400a5
belittlemad <68607967+belittlemad@users.noreply.github.com>
2022-11-11 19:46:31 +0530

    Update tracker_app.py

--- tracker_app.py
+++ tracker_app.py
@@ -4,5 +4,5 @@ st.header("test html import")
 
 HtmlFile = open("test.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
-print(source_code)
+st.write(source_code)
 components.html(source_code)

500dbfe1cc47a3939ee2877704062291009f7a8a
belittlemad <68607967+belittlemad@users.noreply.github.com>
2022-11-11 19:39:37 +0530

    Update tracker_app.py

--- tracker_app.py
+++ tracker_app.py
@@ -1,3 +1,8 @@
-import geocoder
-g = geocoder.ip('me')
-print(g.latlng)
+import streamlit as st
+import streamlit.components.v1 as components
+st.header("test html import")
+
+HtmlFile = open("test.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+print(source_code)
+components.html(source_code)
